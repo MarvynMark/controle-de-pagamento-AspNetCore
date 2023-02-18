2c83fb95af5d26ae92398242fa73f422cd3d6d1c
Marvyn Mark <marvyn.mark@gmail.com>
2023-02-17 21:38:06 -0300

    Retirado view Home deixando somente a view de ImportacaoArquivos.

--- src/ControleDePagamento.Web/Program.cs
+++ src/ControleDePagamento.Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ImportacaoArquivos}/{action=Index}/{id?}");
 
 app.Run();
